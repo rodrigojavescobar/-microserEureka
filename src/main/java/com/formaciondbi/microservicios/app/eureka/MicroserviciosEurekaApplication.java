5044d33313a241965039024da9fe953b2d10131a
Rodrigo Javier Escobar <157421920+rodrigojavescobar@users.noreply.github.com>
2024-01-23 18:25:48 -0500

    Update MicroserviciosEurekaApplication.java
    para descarga

--- src/main/java/com/formaciondbi/microservicios/app/eureka/MicroserviciosEurekaApplication.java
+++ src/main/java/com/formaciondbi/microservicios/app/eureka/MicroserviciosEurekaApplication.java
@@ -2,7 +2,7 @@ package com.formaciondbi.microservicios.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; Cambio a mal
 
 @EnableEurekaServer
 @SpringBootApplication
